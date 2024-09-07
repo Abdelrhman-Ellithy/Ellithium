88abde948c0f9ed9fb078ffb0d3476d24faebbaf
Abdelrhman-Ellithy <https://github.com/Abdelrhman-Ellithy>
2024-09-07 18:37:39 +0300

    update command executer logic

--- src/main/java/AutoEllithiumSphere/Utilities/CommandExecutor.java
+++ src/main/java/AutoEllithiumSphere/Utilities/CommandExecutor.java
@@ -6,17 +6,13 @@ import java.io.InputStreamReader;
 
 public class CommandExecutor {
 
-    // This method will execute a given command in the terminal
     public static void executeCommand(String command) {
         try {
-            // Use ProcessBuilder to run the command
             ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", command);
             builder.redirectErrorStream(true);
 
-            // Start the process
             Process process = builder.start();
 
-            // Wait for the process to complete
             int exitCode = process.waitFor();
             System.out.println("\nCommand exited with code: " + exitCode);
 
