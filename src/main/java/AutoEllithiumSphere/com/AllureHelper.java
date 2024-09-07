e10f98d7f45ac5183aefbc5218c8ad937a3488c6
Abdelrhman-Ellithy <https://github.com/Abdelrhman-Ellithy>
2024-09-07 18:37:39 +0300

    update command executer logic

--- src/main/java/AutoEllithiumSphere/com/AllureHelper.java
+++ src/main/java/AutoEllithiumSphere/com/AllureHelper.java
@@ -8,24 +8,21 @@ import java.io.File;
 public class AllureHelper {
 
     public static void allureOpen(){
-        // Properly construct the path using File.separator
+
         String propertiesFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator
                 + "main" + File.separator + "resources" + File.separator + "properties" + File.separator + "default"
                 + File.separator + "allure";
 
-        // Fetch the flag for allure report opening
         String openFlag = PropertyHelper.getDataFromProperties(propertiesFilePath, "allure.open.afterExecution");
 
-        // Check if the flag is true, then execute the report generation command
         if(openFlag.equalsIgnoreCase("true")){
-            // Define commands for generating and opening the Allure report
+
             String commands = String.join(
                     " && ",
                     "allure generate Test-Output/Reports/Allure/allure-results --clean -o Test-Output/Reports/Allure/allure-report"
                     ,"allure open Test-Output/Reports/Allure/allure-report"
             );
 
-            // Execute the commands using CommandExecutor
             CommandExecutor.executeCommand(commands);
         }
     }
