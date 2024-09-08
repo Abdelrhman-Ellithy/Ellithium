5075c7b1fced475bffadc5f359bb978ab4c1bc77
Abdelrhman-Ellithy <https://github.com/Abdelrhman-Ellithy>
2024-09-08 10:07:52 +0300

    update

--- src/main/java/AutoEllithiumSphere/com/AllureHelper.java
+++ src/main/java/AutoEllithiumSphere/com/AllureHelper.java
@@ -9,20 +9,20 @@ public class AllureHelper {
 
     public static void allureOpen(){
 
-        String propertiesFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator
+        String propertiesFilePath = System.getProperty("user.dir")+ File.separator + "src" + File.separator
                 + "main" + File.separator + "resources" + File.separator + "properties" + File.separator + "default"
                 + File.separator + "allure";
 
         String openFlag = PropertyHelper.getDataFromProperties(propertiesFilePath, "allure.open.afterExecution");
-
+        String allureBinary =".allure" + File.separator +"allure-2.30.0" +File.separator+ "bin"
+                + File.separator+"allure ";
         if(openFlag.equalsIgnoreCase("true")){
 
             String commands = String.join(
                     " && ",
-                    "allure generate Test-Output/Reports/Allure/allure-results --clean -o Test-Output/Reports/Allure/allure-report"
-                    ,"allure open Test-Output/Reports/Allure/allure-report"
+                    allureBinary+" generate Test-Output/Reports/Allure/allure-results --clean -o Test-Output/Reports/Allure/allure-report"
+                    ,allureBinary+"open Test-Output/Reports/Allure/allure-report"
             );
-
             CommandExecutor.executeCommand(commands);
         }
     }
