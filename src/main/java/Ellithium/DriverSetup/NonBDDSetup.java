33d676641c0e342af0a1722ca689bf319c36a6d4
Abdelrhman-Ellithy <https://github.com/Abdelrhman-Ellithy>
2024-09-27 19:13:41 +0300

    update listner

--- src/main/java/Ellithium/DriverSetup/NonBDDSetup.java
+++ src/main/java/Ellithium/DriverSetup/NonBDDSetup.java
@@ -1,9 +1,13 @@
 package Ellithium.DriverSetup;
 
 import Ellithium.Internal.ConfigContext;
+import Ellithium.Internal.CustomTestNGListener;
+import io.qameta.allure.testng.AllureTestNg;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+@Listeners({CustomTestNGListener.class, AllureTestNg.class})
 public class NonBDDSetup {
     @Parameters({"BrowserName","HeadlessMode","PageLoadStrategy","PrivateMode","SandboxMode", "WebSecurityMode" })
     @BeforeTest(alwaysRun = true)
