f1c9744fcc83f1e6aeb5c9badbf5cb76f6193b72
Abdelrhman-Ellithy <abdelarhmanellithy@gmail.com>
2024-10-30 19:35:30 +0300

    update readme

---
+++ src/main/java/Ellithium/core/DB/SQLConnectors.java
@@ -0,0 +1,13 @@
+package Ellithium.core.DB;
+
+public class SQLConnectors {
+    private static String userName;
+    private static String password;
+    private static String port;
+    private static String connectionString;
+    private static String dataBaseName;
+    private static String serverIP;
+    public SQLConnectors(String userName, String password, String port,String connectionString,String dataBaseName,String serverIP){
+        t
+    }
+}
