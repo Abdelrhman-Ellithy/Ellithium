63ec1d71ca8a221e4d5765dbd8fc55748a941876
Abdelrhman-Ellithy <abdelarhmanellithy@gmail.com>
2025-03-06 22:36:11 +0200

    Add Note Tests

--- src/test/java/Pages/NotesHomePage.java
+++ src/test/java/Pages/NotesHomePage.java
@@ -1,2 +1,20 @@
-package Pages;public class NotesHomePage {
+package Pages;
+
+import Ellithium.Utilities.interactions.DriverActions;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+
+public class NotesHomePage {
+    private AndroidDriver driver;
+    private DriverActions driverActions;
+    public NotesHomePage(AndroidDriver driver){
+        this.driver=driver;
+        driverActions=new DriverActions<>(this.driver);
+    }
+
+    public NotePage addNote(){
+        driverActions.clickOnElement(AppiumBy.id("com.miui.notes:id/content_add"));
+        return new NotePage(driver);
+    }
 }
