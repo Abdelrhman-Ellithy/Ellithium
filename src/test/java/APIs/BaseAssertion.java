118b27e5ec2dfa787e9037cc0d4cc2d85e985192
Abdelrhman-Ellithy <abdelarhmanellithy@gmail.com>
2025-11-14 19:15:34 +0200

    release 2.2.2

--- src/test/java/APIs/BaseAssertion.java
+++ src/test/java/APIs/BaseAssertion.java
@@ -1,7 +1,6 @@
 package APIs;
 
 import Ellithium.Utilities.assertion.AssertionExecutor;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseAssertion {
